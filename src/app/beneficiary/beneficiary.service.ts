ddbc017a8cc6eaf6a1487a4bbf412c2ea1936f1d
perolanegra <pedratto3@gmail.com>
2023-12-11 15:46:27 +0000

    rolling back to remove mock

--- src/app/beneficiary/beneficiary.service.ts
+++ src/app/beneficiary/beneficiary.service.ts
@@ -54,10 +54,10 @@ export function filterNullish<T>(): UnaryFunction<
   providedIn: 'root',
 })
 export class BeneficiaryService {
-  private url = 'http://localhost:3000';
+  private url = 'http://localhost:8080';
   http = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
-  private hasMock = true;
+  private hasMock = false;
 
   private readonly beneficiary = signal<any>({} as Beneficiary);
   private readonly _selectedbeneficiary = signal<any>({} as Beneficiary);
